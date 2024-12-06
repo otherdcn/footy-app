9a7dddd42dcdc4781861c205e5925000cbb0c1b0
Javier Rossetti <javierrossetti12@gmail.com>
2024-12-06 14:02:36 +0100

    Refactor spec

--- spec/helpers/countries_helper_spec.rb
+++ spec/helpers/countries_helper_spec.rb
@@ -2,12 +2,10 @@ require 'rails_helper'
 
 RSpec.describe CountriesHelper, type: :helper do
   describe "#countries" do
-    # def get_countries(country_code: nil)
-    #   countries_data = CountriesHelper::CountryData.new
-    #   options = country_code.nil? ? {} : { "code" => country_code.to_s }
 
-    #   countries_data.countries(options)
-    # end
+    # lets add VCR
+    # make the call to the api and store the response in a cassette
+    # use the cassette to test the helper method
 
     subject { CountriesHelper::CountryData.new }
     let(:country_code) { nil }
@@ -34,33 +32,5 @@ RSpec.describe CountriesHelper, type: :helper do
         expect(name_of_country).to eq "Namibia"
       end
     end
-
-
-    # context "when no arguments are given" do
-
-    #   it "returns all the countries" do
-    #     countries = get_countries(country_code: nil)
-
-    #     number_of_responses = countries["response"].size
-
-    #     expect(number_of_responses).to be > 2
-
-    #   end
-
-    # end
-
-    # context "when arguments are given" do
-
-    #   it "returns only the specified country requested" do
-    #     countries = get_countries(country_code: "na")
-
-    #     number_of_responses = countries["response"].size
-    #     name_of_country = countries["response"][0]["name"]
-
-    #     expect(number_of_responses).to be == 1
-    #     expect(name_of_country).to eq "Namibia"
-    #   end
-
-    # end
   end
 end
