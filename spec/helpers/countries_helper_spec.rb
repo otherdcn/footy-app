62116b7832f9d0b9abed9af2e1cf86afd8a8d3f3
Javier Rossetti <javierrossetti12@gmail.com>
2024-12-06 13:56:34 +0100

    Changing CountriesHelper

--- spec/helpers/countries_helper_spec.rb
+++ spec/helpers/countries_helper_spec.rb
@@ -2,30 +2,30 @@ require 'rails_helper'
 
 RSpec.describe CountriesHelper, type: :helper do
   describe "#countries" do
-    def get_countries(country_code: nil)
-      countries_data = CountriesHelper::CountryData.new
-      options = country_code.nil? ? {} : { "code" => country_code.to_s }
+    # def get_countries(country_code: nil)
+    #   countries_data = CountriesHelper::CountryData.new
+    #   options = country_code.nil? ? {} : { "code" => country_code.to_s }
 
-      countries_data.countries(options)
+    #   countries_data.countries(options)
+    # end
+
+    subject { CountriesHelper::CountryData.new }
+    let(:country_code) { nil }
+    let(:options) do
+      country_code.nil? ? {} : { "code" => country_code.to_s }
     end
 
-    context "when no arguments are given" do
-
+    context "when country_code is nil" do
       it "returns all the countries" do
-        countries = get_countries(country_code: nil)
-
-        number_of_responses = countries["response"].size
-
-        expect(number_of_responses).to be > 2
-
+        subject.countries(options)["response"].to be > 2
       end
-
     end
 
-    context "when arguments are given" do
+    context "when country_code is not nil" do
+      let(:country_code) { "na" }
 
       it "returns only the specified country requested" do
-        countries = get_countries(country_code: "na")
+        countries = subject.countries(options)
 
         number_of_responses = countries["response"].size
         name_of_country = countries["response"][0]["name"]
@@ -33,7 +33,34 @@ RSpec.describe CountriesHelper, type: :helper do
         expect(number_of_responses).to be == 1
         expect(name_of_country).to eq "Namibia"
       end
-
     end
+
+
+    # context "when no arguments are given" do
+
+    #   it "returns all the countries" do
+    #     countries = get_countries(country_code: nil)
+
+    #     number_of_responses = countries["response"].size
+
+    #     expect(number_of_responses).to be > 2
+
+    #   end
+
+    # end
+
+    # context "when arguments are given" do
+
+    #   it "returns only the specified country requested" do
+    #     countries = get_countries(country_code: "na")
+
+    #     number_of_responses = countries["response"].size
+    #     name_of_country = countries["response"][0]["name"]
+
+    #     expect(number_of_responses).to be == 1
+    #     expect(name_of_country).to eq "Namibia"
+    #   end
+
+    # end
   end
 end
