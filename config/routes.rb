42e6898cb8ccc22a0195ee7aaaefb0749e464dba
otherdcn <danny.c.ntwari@gmail.com>
2025-02-18 20:45:29 +0200

    Install and configure Devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  devise_for :users, controllers: {
+    registrations: "users/registrations",
+  }
+  #get "up" => "rails/health#show", as: :rails_health_check
 
   root "countries#index"
   resources "countries",  only: [:index, :show]
