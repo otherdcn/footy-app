74bf0c7d49cd44b756890314b23f7e30c78f9c11
otherdcn <danny.c.ntwari@gmail.com>
2025-02-18 13:05:46 +0200

    Edit model user username attribute
    Set default value after initializing user object from substring of
    email. Display username in root_path. Remove inclusion of username field
    from registraion page.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  after_initialize :set_defaults
+
+  validates_presence_of :username
+
+  private
+
+  def set_defaults
+    self.username ||= self.email.split("@").first
+  end
 end
